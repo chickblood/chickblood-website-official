0b9d3a1d82b9f1503b7854aa4e9d8cd568daa0f9
Yuxuan Shi <1051268839@qq.com>
2024-05-20 14:43:24 +0800

    Update LandingPage.js
    update Type your question

--- chickblood-test/src/components/LandingPage.jsx
+++ chickblood-test/src/components/LandingPage.jsx
@@ -1,10 +1,86 @@
 import React from "react";
+import { Box, Grid, TextField } from "@mui/material";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+
+// First Layer, type your question
+
+const customTheme = (outerTheme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "--TextField-brandBorderColor": "#E0E3E7",
+            "--TextField-brandBorderHoverColor": "#B2BAC2",
+            "--TextField-brandBorderFocusedColor": "#6F7E8C",
+            "& label.Mui-focused": {
+              color: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: "var(--TextField-brandBorderColor)",
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderHoverColor)",
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+          input: {
+            fontSize: "0.8rem", // Change this to your desired font size
+            fontFamily: "monospace", // Change this to your desired font family
+          },
+        },
+      },
+      MuiInputLabel: {
+        styleOverrides: {
+          root: {
+            fontSize: "0.8rem",
+            fontFamily: "monospace",
+          },
+        },
+      },
+    },
+  });
 
 function LandingPage() {
+  const outerTheme = useTheme();
   return (
     <div>
-      <h2>this is our landing page</h2>
-      helloworld
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "auto",
+            }}
+          >
+            <ThemeProvider theme={customTheme(outerTheme)}>
+              <TextField label="Type Your Question" />
+            </ThemeProvider>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 }
