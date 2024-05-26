69fa7101bf6a75c636bcb712e6078a64561a4b05
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-05-26 18:59:18 +0800

    Update Soundcloud Embed
    - this one is much better

--- chickblood-test/src/beta/Playground.jsx
+++ chickblood-test/src/beta/Playground.jsx
@@ -2,6 +2,7 @@ import { Box, Grid, Typography } from "@mui/material";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import HideableDrawer from "../utils/HideableDrawer";
+import ReactPlayer from "react-player";
 
 export default function Playground() {
   return (
@@ -74,7 +75,7 @@ export default function Playground() {
             }}
           >
             <Typography sx={{ fontFamily: "monospace", fontSize: "14px" }}>
-              Spotify Embed
+              Soundcloud & Spotify Embed
             </Typography>
           </Box>
           {/* bootstrap embed ratio is not used */}
@@ -85,11 +86,44 @@ export default function Playground() {
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              height: "80vh",
+              height: "40vh",
               width: "100%",
             }}
           >
-            <SpotifyEmbed></SpotifyEmbed>
+            <iframe
+              style={{ "border-radius": "12px" }}
+              width="100%"
+              height="100%"
+              frameborder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1080371245&color=%23363636&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+            ></iframe>
+          </Box>
+          {/* -------------------- End of Spotify Embed -------------------- */}
+          {/* -------------------- Soundcloud Embed -------------------- */}
+          <Box
+            p={0.5}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "180px",
+              width: "100%",
+            }}
+          >
+            <SpotifyEmbed1></SpotifyEmbed1>
+          </Box>
+          <Box
+            p={0.5}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "180px",
+              width: "100%",
+            }}
+          >
+            <SpotifyEmbed2></SpotifyEmbed2>
           </Box>
           {/* </div> */}
           {/* -------------------- End of Spotify Embed -------------------- */}
@@ -102,12 +136,38 @@ export default function Playground() {
 /**
  * Spotify Embed is self-responsive and is based on pixels. Works fine on laptop but 45vh triggers smaller window on ipad
  *  **/
-const SpotifyEmbed = () => {
+const SpotifyEmbed1 = () => {
   const embedCode = `<iframe style="border-radius:12px" 
-  src="https://open.spotify.com/embed/playlist/3TYCQYrMRi9q6jCh23MogZ?utm_source=generator" 
+  src="https://open.spotify.com/embed/playlist/1nHHxMzRJVaD6Cru1okKxd?utm_source=generator" 
   width="100%" height="100%" frameBorder="0" allow="autoplay; 
   clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
   loading="lazy"></iframe>`;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+      }}
+      dangerouslySetInnerHTML={{ __html: embedCode }}
+    ></Box>
+  );
+};
+
+const SpotifyEmbed2 = () => {
+  const embedCode = `<iframe
+  style="border-radius:12px"
+  src="https://open.spotify.com/embed/playlist/3TYCQYrMRi9q6jCh23MogZ?utm_source=generator"
+  width="100%"
+  height="100%"
+  frameBorder="0"
+  allowfullscreen=""
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+></iframe>;`;
 
   return (
     <Box
