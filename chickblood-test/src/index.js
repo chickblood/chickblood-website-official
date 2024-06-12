91d4b5e4713f9cbefcf5a245e88d3339176e2bd0
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-06-12 12:17:47 +0800

    Update Member Carousel
    - Note that an .env file is added to suppress warnings on source map warnings

--- chickblood-test/src/index.js
+++ chickblood-test/src/index.js
@@ -9,13 +9,13 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <React.Fragment>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.Fragment>
+  // <React.Fragment>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+  // </React.Fragment>
   // </React.StrictMode>
 );
 
