ed5e6236bad4a8c7ca282963195182afd69f3a55
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-06-14 15:02:18 +0800

    Update Falling Ball Test
    - Added falling ball test, main functionalities are implemented.
    - Added slight angular velocity upon issue ball's generation
    - Added gravity system pointing towards center of page
    - Added transparent border to the screen frame so balls won't escape

--- chickblood-test/src/beta/FallingTest.jsx
+++ chickblood-test/src/beta/FallingTest.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import FallingBalls from "./FallingBalls";
+import { Box } from "@mui/material";
+import HideableDrawer from "../utils/HideableDrawer";
 
 export default function FallingTest() {
   return (
     <div>
+      <Box
+        position="absolute"
+        sx={{ marginLeft: "1%", marginTop: "1%", zIndex: 1000 }}
+      >
+        <HideableDrawer />
+      </Box>
       <FallingBalls></FallingBalls>
     </div>
   );
