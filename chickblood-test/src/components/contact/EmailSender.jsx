7e6b9d43ae935ecca5be51f35b5acf6bca7c988c
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-06-14 20:10:39 +0800

    Initialize email sender
    - Planning on using emailJS

---
+++ chickblood-test/src/components/contact/EmailSender.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Paper } from "@mui/material";
+
+export default function EmailSender() {
+  return (
+    <Paper
+      elevation={12}
+      sx={{ borderRadius: 4, width: "100%", height: "100%", padding: 2 }}
+    >
+      <Box border={1}>hello</Box>
+    </Paper>
+  );
+}
