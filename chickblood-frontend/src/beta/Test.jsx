fca2b66fbc1f1b656aceea10eb6bb94c8e68f637
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-07-03 12:05:46 +0800

    Update Test.jsx

--- chickblood-frontend/src/beta/Test.jsx
+++ chickblood-frontend/src/beta/Test.jsx
@@ -1,63 +1,46 @@
-import React, { useState } from "react";
-import { useTranslation } from "react-i18next";
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 export default function Test() {
-  const [content, setContent] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(false);
+
+  const handleLoad = () => {
+    setLoading(false);
+    setError(false);
+  };
+
+  const handleError = () => {
+    setLoading(false);
+    setError(true);
+  };
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          setContent(1);
+    <Box>
+      {loading && <div>Loading...</div>}
+      {error && <div>Error loading iframe.</div>}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
         }}
+        p={8}
       >
-        content 1
-      </button>
-      <button
-        onClick={() => {
-          setContent(2);
-        }}
-      >
-        content 2
-      </button>
-      <button
-        onClick={() => {
-          setContent(3);
-        }}
-      >
-        content 3
-      </button>
-      <div className="content">
-        {content === 1 ? (
-          <Section1 />
-        ) : content === 2 ? (
-          <Section2 />
-        ) : content === 3 ? (
-          <Section3 />
-        ) : null}
-      </div>
-    </div>
+        <iframe
+          src="https://open.spotify.com/embed/playlist/1nHHxMzRJVaD6Cru1okKxd?utm_source=generator"
+          onLoad={handleLoad}
+          onError={handleError}
+          style={{
+            display: loading || error ? "none" : "block",
+            width: "100%",
+            height: "500px",
+          }}
+        />
+      </Box>
+    </Box>
   );
 }
-
-const Section1 = () => {
-  const { t } = useTranslation();
-  return (
-    <div>
-      {t("dummycontent1")}
-      <br></br>
-      <br></br>
-      {t("dummycontent1")}
-    </div>
-  );
-};
-
-const Section2 = () => {
-  const { t } = useTranslation();
-  return <div>{t("dummycontent2")}</div>;
-};
-
-const Section3 = () => {
-  const { t } = useTranslation();
-  return <div>{t("dummycontent3")}</div>;
-};
