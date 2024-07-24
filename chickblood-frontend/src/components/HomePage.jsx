188ce3149c64c26f953d0de72ada679125754556
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-07-24 16:48:25 +0800

    Update App Bar

--- chickblood-frontend/src/components/HomePage.jsx
+++ chickblood-frontend/src/components/HomePage.jsx
@@ -1,14 +1,11 @@
 import { Box } from "@mui/material";
 import React from "react";
-import HideableDrawer from "../utils/HideableDrawer";
-import Construction from "../beta/Construction";
+import CBAppBar from "../utils/CBAppBar";
 
 function HomePage() {
   return (
     <Box height="100vh" position="relative" style={{ overflow: "hidden" }}>
-      <Box position="absolute" sx={{ marginLeft: "1%", marginTop: "1%" }}>
-        <HideableDrawer />
-      </Box>
+      <CBAppBar></CBAppBar>
     </Box>
   );
 }
