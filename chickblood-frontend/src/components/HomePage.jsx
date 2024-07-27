a56256cbc138858302b0fc44bfbed807bd99e1b0
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-07-27 18:20:38 +0800

    update blog page

--- chickblood-frontend/src/components/HomePage.jsx
+++ chickblood-frontend/src/components/HomePage.jsx
@@ -9,7 +9,7 @@ import LanguageIcon from "../utils/LanguageIcon";
 
 const imageUrls = [
   "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/616c50e0-6502-4279-27e5-4b9c4b379000/public", // background image, office
-  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/b6134cb0-0790-4272-b6c8-09523df6a000/public", // rita holding poster
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/131fc250-1676-4bf9-25b0-665c4e47dc00/public", // rita holding poster
   "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/05ccfda8-59c2-4688-8f74-2c27f103b000/public", // plant
   "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/38d51d5c-ebc9-4466-079c-8852ac22fb00/public", // language icon
 ];
@@ -99,7 +99,7 @@ function HomePage() {
             }}
           >
             <img
-              src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/b6134cb0-0790-4272-b6c8-09523df6a000/public"
+              src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/131fc250-1676-4bf9-25b0-665c4e47dc00/public"
               alt="rita"
               style={{
                 height: "100%",
