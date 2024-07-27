43736b74054ca7e9e469686836c2b5c48a2357b7
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-07-27 16:51:41 +0800

    update build

---
+++ chickblood-frontend/src/components/blog/generationalBruise/GenerationalBruise.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function GenerationalBruise() {
+  return (
+    <div style={{ backgroundColor: "#fffcc4", height: "100vh" }}>
+      <Box sx={{ height: "80px" }} border={1}>
+        joshua
+      </Box>
+    </div>
+  );
+}
