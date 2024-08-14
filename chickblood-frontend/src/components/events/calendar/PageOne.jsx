252fd67902aa6956f882339fd67e3d03ac02ec6f
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-08-14 15:36:31 +0800

    update calendar img optimization

--- chickblood-frontend/src/components/events/calendar/PageOne.jsx
+++ chickblood-frontend/src/components/events/calendar/PageOne.jsx
@@ -1,7 +1,56 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import LoadingPage from "../../../utils/LoadingPage";
+
+const imageUrls = [
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/f5d051d1-dce7-4d22-2be1-d4edb587d500/public", //1
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2c0d0444-bd69-454b-4f39-95f5e6389100/public", //2
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2fafd9df-59db-4702-fc27-f508e4e78500/public", //3
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/ee0cfdf0-643d-4bc1-d3c7-21b75f47e600/public", //4
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/593ce91a-cac9-4286-fe45-8f541aaa8e00/public", //5
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/db78968f-cef7-457b-2562-8ac0c08ba000/public", //6
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2d8eec05-60dc-4286-fb91-762690c8f000/public", //7
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/ae3e72f7-5a30-4783-a164-077470728400/public", //8
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/494891cc-13fe-4092-6be8-d4b5d7a91f00/public", //9
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/88faf6f2-4ebb-4efa-6d27-4d33c47ad900/public", //10
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/8b4cc9d4-9615-4ec1-fe83-497baa938700/public", //11
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/d7143b51-8f56-4cad-1f32-365dc3f26a00/public", //12
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/7378e9d1-5587-4830-07dd-436023d26700/public", //13
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/9839ef1f-f453-4e1c-6050-6cab4138dd00/public", //14
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/52821144-b583-4e2b-61ba-e93631daef00/public", //15
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/92262acc-b131-435b-7e47-9cd590a14900/public", //16
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/041da201-95d2-417e-4947-f704603c7600/public", //page01
+];
 
 export default function PageOne() {
+  // image preload
+  const [openLoader, setOpenLoader] = useState(true);
+  const handleCloseLoader = () => {
+    setOpenLoader(false);
+  };
+  // const handleOpenLoader = () => {
+  //   setOpenLoader(true);
+  // };
+  const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    });
+  };
+  useEffect(() => {
+    const preloadImages = async () => {
+      try {
+        await Promise.all(imageUrls.map((url) => loadImage(url)));
+        setOpenLoader(false);
+      } catch (error) {
+        console.error("Error loading images:", error);
+      }
+    };
+
+    preloadImages();
+  }, []);
   // glitch effect
   const glitchAnimation = `
     @keyframes glitch {
@@ -26,9 +75,14 @@ export default function PageOne() {
   };
   return (
     <Box sx={{ position: "relative", height: "600px", width: "auto" }}>
+      {/* Loading Page */}
+      <LoadingPage
+        openLoader={openLoader}
+        handleClose={handleCloseLoader}
+      ></LoadingPage>
       {/* background image */}
       <img
-        src="src/assets/pics/calendar/page01.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/041da201-95d2-417e-4947-f704603c7600/public"
         alt="page one bg"
         style={{
           height: "600px",
@@ -38,7 +92,7 @@ export default function PageOne() {
       />
       {/* 16 */}
       <img
-        src="src/assets/pics/calendar/16.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/92262acc-b131-435b-7e47-9cd590a14900/public"
         alt="16"
         style={{
           position: "absolute",
@@ -52,7 +106,7 @@ export default function PageOne() {
       />
       {/* 15 */}
       <img
-        src="src/assets/pics/calendar/15.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/52821144-b583-4e2b-61ba-e93631daef00/public"
         alt="15"
         style={{
           position: "absolute",
@@ -66,7 +120,7 @@ export default function PageOne() {
       />
       {/* 14 */}
       <img
-        src="src/assets/pics/calendar/14.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/9839ef1f-f453-4e1c-6050-6cab4138dd00/public"
         alt="14"
         style={{
           position: "absolute",
@@ -80,7 +134,7 @@ export default function PageOne() {
       />
       {/* 13 */}
       <img
-        src="src/assets/pics/calendar/13.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/7378e9d1-5587-4830-07dd-436023d26700/public"
         alt="13"
         style={{
           position: "absolute",
@@ -94,7 +148,7 @@ export default function PageOne() {
       />
       {/* 12 */}
       <img
-        src="src/assets/pics/calendar/12.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/d7143b51-8f56-4cad-1f32-365dc3f26a00/public"
         alt="12"
         style={{
           position: "absolute",
@@ -108,7 +162,7 @@ export default function PageOne() {
       />
       {/* 11 */}
       <img
-        src="src/assets/pics/calendar/11.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/8b4cc9d4-9615-4ec1-fe83-497baa938700/public"
         alt="11"
         style={{
           position: "absolute",
@@ -122,7 +176,7 @@ export default function PageOne() {
       />
       {/* 10 */}
       <img
-        src="src/assets/pics/calendar/10.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/88faf6f2-4ebb-4efa-6d27-4d33c47ad900/public"
         alt="10"
         style={{
           position: "absolute",
@@ -136,7 +190,7 @@ export default function PageOne() {
       />
       {/* 9 */}
       <img
-        src="src/assets/pics/calendar/9.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/494891cc-13fe-4092-6be8-d4b5d7a91f00/public"
         alt="9"
         style={{
           position: "absolute",
@@ -150,7 +204,7 @@ export default function PageOne() {
       />
       {/* 8 */}
       <img
-        src="src/assets/pics/calendar/8.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/ae3e72f7-5a30-4783-a164-077470728400/public"
         alt="8"
         style={{
           position: "absolute",
@@ -164,7 +218,7 @@ export default function PageOne() {
       />
       {/* 7 */}
       <img
-        src="src/assets/pics/calendar/7.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2d8eec05-60dc-4286-fb91-762690c8f000/public"
         alt="7"
         style={{
           position: "absolute",
@@ -178,13 +232,83 @@ export default function PageOne() {
       />
       {/* 6 */}
       <img
-        src="src/assets/pics/calendar/6.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/db78968f-cef7-457b-2562-8ac0c08ba000/public"
         alt="6"
         style={{
           position: "absolute",
           top: "305px",
-          left: "190px",
+          left: "185px",
           height: "160px",
+          width: "auto",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      {/* 5 */}
+      <img
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/593ce91a-cac9-4286-fe45-8f541aaa8e00/public"
+        alt="5"
+        style={{
+          position: "absolute",
+          top: "315px",
+          left: "260px",
+          height: "100px",
+          width: "auto",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      {/* 4 */}
+      <img
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/ee0cfdf0-643d-4bc1-d3c7-21b75f47e600/public"
+        alt="4"
+        style={{
+          position: "absolute",
+          top: "400px",
+          left: "265px",
+          height: "65px",
+          width: "auto",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      {/* 3 */}
+      <img
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2fafd9df-59db-4702-fc27-f508e4e78500/public"
+        alt="3"
+        style={{
+          position: "absolute",
+          top: "445px",
+          left: "20px",
+          height: "80px",
+          width: "auto",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      {/* 2 */}
+      <img
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/2c0d0444-bd69-454b-4f39-95f5e6389100/public"
+        alt="2"
+        style={{
+          position: "absolute",
+          top: "455px",
+          left: "125px",
+          height: "90px",
+          width: "auto",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      />
+      {/* 1 */}
+      <img
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/f5d051d1-dce7-4d22-2be1-d4edb587d500/public"
+        alt="1"
+        style={{
+          position: "absolute",
+          top: "450px",
+          left: "250px",
+          height: "100px",
           width: "auto",
         }}
         onMouseEnter={handleMouseEnter}
--- chickblood-frontend/src/components/events/calendar/PageTwo.jsx
+++ chickblood-frontend/src/components/events/calendar/PageTwo.jsx
@@ -1,6 +1,25 @@
 import { Box } from "@mui/material";
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import LoadingPage from "../../../utils/LoadingPage";
+
+const imageUrls = [
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/11dfe8e5-83df-455a-6e37-8ea75a45b100/public", //17
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/70925860-005f-4fdc-7a2a-545fee452500/public", //18
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/e964609d-cbc8-4c00-b03b-9ebaa00c1100/public", //19
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/90266000-5450-40cc-6a83-67d8df48e200/public", //20
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/66895882-0251-4804-49fe-44dfcd21ce00/public", //21
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/c2e385d9-b942-485c-2414-1569df2a7b00/public", //22
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/8c59f196-a627-416a-7560-f4d291cad500/public", //23
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/50a06b79-dc86-44e6-c39a-343b27925c00/public", //24
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/9414ec65-9715-421f-ebca-386cb8dda300/public", //25
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/32e60221-4963-4c1c-d846-91035e10da00/public", //26
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/6683ecfd-0007-4bbe-434a-01e616dc9d00/public", //27
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/5a91ac82-ff96-4837-56b5-ce31b387f500/public", //28
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/db5b9ad2-060e-4df5-db0b-63015c8c8200/public", //29
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/884dae8b-bd22-4641-5dde-01a053724c00/public", //30
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/7db9d271-ed74-4d66-21dc-491b54ce6100/public", //31
+  "https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/04ab2540-0b12-4acc-6cf2-3cbc17a74100/public", //page 02
+];
 
 export default function PageTwo() {
   // glitch effect
@@ -25,12 +44,45 @@ export default function PageTwo() {
   const handleMouseLeave = (e) => {
     e.target.style.animation = "none";
   };
+  // image preload
+  const [openLoader, setOpenLoader] = useState(true);
+  const handleCloseLoader = () => {
+    setOpenLoader(false);
+  };
+  // const handleOpenLoader = () => {
+  //   setOpenLoader(true);
+  // };
+  const loadImage = (src) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    });
+  };
+  useEffect(() => {
+    const preloadImages = async () => {
+      try {
+        await Promise.all(imageUrls.map((url) => loadImage(url)));
+        setOpenLoader(false);
+      } catch (error) {
+        console.error("Error loading images:", error);
+      }
+    };
+
+    preloadImages();
+  }, []);
 
   return (
     <Box sx={{ position: "relative", height: "600px", width: "auto" }}>
+      {/* Loading Page */}
+      <LoadingPage
+        openLoader={openLoader}
+        handleClose={handleCloseLoader}
+      ></LoadingPage>
       {/* background image */}
       <img
-        src="src/assets/pics/calendar/page02.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/04ab2540-0b12-4acc-6cf2-3cbc17a74100/public"
         alt="page two bg"
         style={{
           height: "600px",
@@ -40,7 +92,7 @@ export default function PageTwo() {
       />
       {/* 31 */}
       <img
-        src="src/assets/pics/calendar/31.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/7db9d271-ed74-4d66-21dc-491b54ce6100/public"
         alt="31"
         style={{
           position: "absolute",
@@ -54,7 +106,7 @@ export default function PageTwo() {
       />
       {/* 30 */}
       <img
-        src="src/assets/pics/calendar/30.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/884dae8b-bd22-4641-5dde-01a053724c00/public"
         alt="30"
         style={{
           position: "absolute",
@@ -68,7 +120,7 @@ export default function PageTwo() {
       />
       {/* 29 */}
       <img
-        src="src/assets/pics/calendar/29.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/db5b9ad2-060e-4df5-db0b-63015c8c8200/public"
         alt="29"
         style={{
           position: "absolute",
@@ -82,7 +134,7 @@ export default function PageTwo() {
       />
       {/* 28 */}
       <img
-        src="src/assets/pics/calendar/28.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/5a91ac82-ff96-4837-56b5-ce31b387f500/public"
         alt="28"
         style={{
           position: "absolute",
@@ -96,7 +148,7 @@ export default function PageTwo() {
       />
       {/* 27 */}
       <img
-        src="src/assets/pics/calendar/28.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/6683ecfd-0007-4bbe-434a-01e616dc9d00/public"
         alt="27"
         style={{
           position: "absolute",
@@ -110,7 +162,7 @@ export default function PageTwo() {
       />
       {/* 26 */}
       <img
-        src="src/assets/pics/calendar/26.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/32e60221-4963-4c1c-d846-91035e10da00/public"
         alt="26"
         style={{
           position: "absolute",
@@ -124,7 +176,7 @@ export default function PageTwo() {
       />
       {/* 25 */}
       <img
-        src="src/assets/pics/calendar/25.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/9414ec65-9715-421f-ebca-386cb8dda300/public"
         alt="25"
         style={{
           position: "absolute",
@@ -138,7 +190,7 @@ export default function PageTwo() {
       />
       {/* 24 */}
       <img
-        src="src/assets/pics/calendar/24.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/50a06b79-dc86-44e6-c39a-343b27925c00/public"
         alt="24"
         style={{
           position: "absolute",
@@ -152,7 +204,7 @@ export default function PageTwo() {
       />
       {/* 23 */}
       <img
-        src="src/assets/pics/calendar/23.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/8c59f196-a627-416a-7560-f4d291cad500/public"
         alt="23"
         style={{
           position: "absolute",
@@ -166,7 +218,7 @@ export default function PageTwo() {
       />
       {/* 22 */}
       <img
-        src="src/assets/pics/calendar/22.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/c2e385d9-b942-485c-2414-1569df2a7b00/public"
         alt="22"
         style={{
           position: "absolute",
@@ -180,7 +232,7 @@ export default function PageTwo() {
       />
       {/* 21 */}
       <img
-        src="src/assets/pics/calendar/21.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/66895882-0251-4804-49fe-44dfcd21ce00/public"
         alt="21"
         style={{
           position: "absolute",
@@ -194,7 +246,7 @@ export default function PageTwo() {
       />
       {/* 20 */}
       <img
-        src="src/assets/pics/calendar/20.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/90266000-5450-40cc-6a83-67d8df48e200/public"
         alt="20"
         style={{
           position: "absolute",
@@ -208,7 +260,7 @@ export default function PageTwo() {
       />
       {/* 19 */}
       <img
-        src="src/assets/pics/calendar/19.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/e964609d-cbc8-4c00-b03b-9ebaa00c1100/public"
         alt="19"
         style={{
           position: "absolute",
@@ -222,7 +274,7 @@ export default function PageTwo() {
       />
       {/* 18 */}
       <img
-        src="src/assets/pics/calendar/18.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/70925860-005f-4fdc-7a2a-545fee452500/public"
         alt="18"
         style={{
           position: "absolute",
@@ -236,7 +288,7 @@ export default function PageTwo() {
       />
       {/* 17 */}
       <img
-        src="src/assets/pics/calendar/17.png"
+        src="https://imagedelivery.net/luUTa6EFyOmipDilm9a3Jw/11dfe8e5-83df-455a-6e37-8ea75a45b100/public"
         alt="17"
         style={{
           position: "absolute",
